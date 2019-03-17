cef5c9d52357ae4332878558d61016c9e084e7af
Steven Tucker <tuxta2@gmail.com>
2019-03-17 17:02:36 +1000

    Set limits on the name that is entered. Only Letters and space, no more than 16 characters, cannot be empty

--- Objects/NameText.py
+++ Objects/NameText.py
@@ -15,114 +15,104 @@ class NameText(TextObject):
 
     def key_pressed(self, key):
         if self.accepting_input:
-            self.accepting_input = False
-            self.set_timer(20, self.accept_input)
 
+            key_recognised = False
             if key[pygame.K_a]:
+                print('Got A')
                 self.text += 'A'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_b]:
                 self.text += 'B'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_c]:
                 self.text += 'C'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_d]:
                 self.text += 'D'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_e]:
                 self.text += 'E'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_f]:
                 self.text += 'F'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_g]:
                 self.text += 'G'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_h]:
                 self.text += 'H'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_i]:
                 self.text += 'I'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_j]:
                 self.text += 'J'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_k]:
                 self.text += 'K'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_l]:
                 self.text += 'L'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_m]:
                 self.text += 'M'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_n]:
                 self.text += 'N'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_o]:
                 self.text += 'O'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_p]:
                 self.text += 'P'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_q]:
                 self.text += 'Q'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_r]:
                 self.text += 'R'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_s]:
                 self.text += 'S'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_t]:
                 self.text += 'T'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_u]:
                 self.text += 'U'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_v]:
                 self.text += 'V'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_w]:
                 self.text += 'W'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_x]:
                 self.text += 'X'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_y]:
                 self.text += 'Y'
-                self.update_text()
-                Globals.player_name = self.text
+                key_recognised = True
             elif key[pygame.K_z]:
                 self.text += 'Z'
-                self.update_text()
-                Globals.player_name = self.text
             elif key[pygame.K_SPACE]:
                 self.text += ' '
+                key_recognised = True
+            elif key[pygame.K_BACKSPACE]:
+                if len(self.text) > 0:
+                    self.text = self.text[:-1]
+                    print(len(self.text))
+                    if len(self.text) == 0:
+                        self.room.invalid_name()
+                    key_recognised = True
+
+            if key_recognised:
+                if len(self.text) > 0:
+                    self.room.valid_name()
+
+                if len(self.text) > 16:
+                    self.text = self.text[:-1]
                 self.update_text()
                 Globals.player_name = self.text
+                self.accepting_input = False
+                self.set_timer(5, self.accept_input)
--- Rooms/EnterName.py
+++ Rooms/EnterName.py
@@ -9,8 +9,16 @@ class EnterName(Level):
         Level.__init__(self, screen, joysticks)
 
         self.set_background_image('ladybug_background.png')
-
         self.add_room_object(TextObject(self, 150, 50, 'Type your name', 80))
-        self.add_room_object(NameText(self, 250, 200))
 
-        self.add_room_object(PlayButton(self, 300, 400))
+        self.add_room_object(NameText(self, 200, 200))
+
+        self.play_button = PlayButton(self, -300, 400)
+        self.add_room_object(self.play_button)
+
+    def valid_name(self):
+        self.play_button.x = 300
+
+    def invalid_name(self):
+        print('Move Play Button')
+        self.play_button.x = 1000
